d2a2db3d40ab99d2b9e4042a5026b7002517f76e
Victor Lai <victor.lai@foxmail.com>
2020-12-07 18:26:48 +0800

    update
    适配最新改动

--- router.go
+++ router.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"neko_server_go"
+	"neko_server_go/core"
+	"neko_server_go/enum"
 	"neko_server_go_example/handler"
 )
 
 var Router = neko_server_go.Router{
-	"/enrollment/view":      handler.EnrollmentView,
-	"/enrollment/add":       handler.EnrollmentAdd,
-	"/enrollment/statistic": handler.EnrollmentStatistic,
-	"/course/add":           handler.CourseAdd,
-	"/course":               handler.CourseAddTemplate,
-	"/":                     handler.Index,
+	"/enrollment/view":      core.CreateMethodsHandler(enum.HttpMethodsGet, handler.EnrollmentView),
+	"/enrollment/add":       core.CreateMethodsHandler(enum.HttpMethodsPost, handler.EnrollmentAdd),
+	"/enrollment/statistic": core.CreateMethodsHandler(enum.HttpMethodsGet, handler.EnrollmentStatistic),
+	"/course/add":           core.CreateMethodsHandler(enum.HttpMethodsPost, handler.CourseAdd),
+	"/course":               core.CreateMethodsHandler(enum.HttpMethodsGet, handler.CourseAddTemplate),
+	"/":                     core.CreateMethodsHandler(enum.HttpMethodsPost, handler.Index),
 }
